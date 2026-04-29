9f29d72041dae6ef41c98c6361bf34687fcc4d58
Anshu thakur <163158412+AnshuThaku@users.noreply.github.com>
2026-04-29 11:32:31 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@ export const removeBackground = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
 
-  const response = await fetch("https://huggingface.co/spaces/Anshutkr/AnshuCut-AI", {
+  const response = await fetch("https://anshutkr-anshucut-ai.hf.space/run/predict", {
     method: "POST",
     body: formData,
   });
