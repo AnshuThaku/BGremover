ed795731aea2671a990500062589d473574a56de
Anshu Kumar <anshthakurcs236@gmail.com>
2026-04-29 10:19:29 +0530

    fixed

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@ export const removeBackground = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
 
-  const response = await fetch("http://127.0.0.1:8000/remove-bg", {
+  const response = await fetch("https://bgremover-bqnu.onrender.com/remove-bg", {
     method: "POST",
     body: formData,
   });
